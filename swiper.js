4d9c9c0b759122bf4b92d23bdfe1ebc1f7a4ea71
Ivan Ryzhov <xXlAlflXx@gmail.com>
2022-11-23 19:42:21 +0300

    3

--- swiper.js
+++ swiper.js
@@ -3,28 +3,6 @@ if (document.documentElement.clientWidth < 768) {
         // Optional parameters
         direction: 'horizontal',
         loop: true,
-
-
-        // If we need pagination
-
-
-        breakpoints: {
-            // when window width is >= 320px
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 20
-            },
-            // when window width is >= 480px
-            480: {
-                slidesPerView: 1,
-                spaceBetween: 30
-            },
-            // when window width is >= 640px
-            640: {
-                slidesPerView: 1,
-                spaceBetween: 40
-            }
-        },
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
